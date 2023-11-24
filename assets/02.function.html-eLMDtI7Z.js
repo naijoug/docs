2d7570ef69b13527d033a7d8e64795c2826fe1e2
naijoug <naijoug@outlook.com>
2023-11-24 09:06:48 +0000

    Deploying to gh-pages from @ naijoug/docs@d4aaea33740f3ec3b26ce6212a2142565a44a1b4 🚀

---
+++ assets/02.function.html-eLMDtI7Z.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as t,c}from"./app-jKljptsy.js";const o={};function n(_,r){return t(),c("div")}const a=e(o,[["render",n],["__file","02.function.html.vue"]]);export{a as default};
