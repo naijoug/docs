d34ebf48cdd14880bef6193a22ea0d1d218fa0d0
naijoug <naijoug@outlook.com>
2023-12-13 16:25:47 +0800

    fix leetcode

--- src/.vuepress/theme/sidebar/interview.ts
+++ src/.vuepress/theme/sidebar/interview.ts
@@ -5,4 +5,12 @@ export const interview = arraySidebar([
     text: "iOS", icon: "hashtag", link: "ios/", prefix: "ios/",
     collapsible: true, children: "structure",
   },
+  {
+    text: "📚", icon: "", link: "book/", prefix: "book/",
+    collapsible: true, children: "structure",
+  },
+  {
+    text: "LeetCode", icon: "hashtag", link: "leetcode/", prefix: "leetcode/",
+    collapsible: true, children: "structure",
+  },
 ])
