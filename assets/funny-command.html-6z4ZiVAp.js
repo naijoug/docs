6c47b6c56208911a217362410d3388c392095953
naijoug <naijoug@outlook.com>
2024-01-17 13:06:32 +0000

    Deploying to gh-pages from @ naijoug/docs@fc230402321572d8246e0ca9e99d1899c3cfa125 🚀

---
+++ assets/funny-command.html-6z4ZiVAp.js
@@ -0,0 +1,48 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as i,o as t,c as l,b as n,e as s,d as o,f as c}from"./app-bX6ZPxEY.js";const m={},r=n("h2",{id:"reference",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#reference","aria-hidden":"true"},"#"),s(" Reference")],-1),p={href:"https://mp.weixin.qq.com/s/d7ZFlBEcXBrMuOMUszE6jA",target:"_blank",rel:"noopener noreferrer"},d=c(`<h2 id="commands" tabindex="-1"><a class="header-anchor" href="#commands" aria-hidden="true">#</a> Commands</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>$ sl
+<span class="token comment"># sl </span>
+<span class="token comment">#   &gt; steam locomotive (蒸汽机车), 一辆蒸汽火车从屏幕经过</span>
+$ cowsay <span class="token string">&quot;ha ha&quot;</span>        <span class="token comment"># 打印奶牛说</span>
+$ cowthink <span class="token string">&quot;what ?&quot;</span>     <span class="token comment"># 打印奶牛想</span>
+$ cowsay <span class="token parameter variable">-f</span> twx <span class="token string">&quot;QQ&quot;</span>    <span class="token comment"># 指定动物  </span>
+<span class="token comment"># cowsay</span>
+<span class="token comment">#   &gt; 打印动物想说的话</span>
+<span class="token comment">#   Options</span>
+<span class="token comment">#       -l : 显示可选动物名称</span>
+<span class="token comment">#       -f : 指定动物的名称</span>
+$ fortune 
+$ fortune <span class="token operator">|</span> cowsay      <span class="token comment"># 奶牛说名言</span>
+<span class="token comment"># fortune</span>
+<span class="token comment">#   &gt; 输出一句名言</span>
+$ cmatrix
+<span class="token comment"># cmatrix</span>
+<span class="token comment">#   &gt; 显示矩阵动画效果</span>
+$ figlet go go          <span class="token comment"># 艺术字显示 &quot;go go&quot; </span>
+<span class="token comment"># figlet</span>
+<span class="token comment">#   &gt; 艺术字生成</span>
+$ toilet I Love You     <span class="token comment"># 默认形式显示  </span>
+$ toilet <span class="token parameter variable">-f</span> mono12 <span class="token parameter variable">-F</span> gay a girl <span class="token comment"># 自定义颜色字体显示</span>
+<span class="token comment">#   &gt; DIY 艺术字生成</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>$ <span class="token function">aptitude</span> moo
+<span class="token comment"># There are no Easter Eggs in this program.</span>
+$ <span class="token function">aptitude</span> <span class="token parameter variable">-v</span> moo
+<span class="token comment"># There really are no Easter Eggs in this program.</span>
+$ <span class="token function">aptitude</span> <span class="token parameter variable">-vv</span> moo
+<span class="token comment"># Didn&#39;t I already tell you that there are no Easter Eggs in this program?</span>
+$ <span class="token function">aptitude</span> <span class="token parameter variable">-vvv</span> moo
+<span class="token comment"># Stop it!</span>
+$ <span class="token function">aptitude</span> <span class="token parameter variable">-vvvv</span> moo
+<span class="token comment"># Okay, okay, if I give you an Easter Egg, will you go away?</span>
+$ <span class="token function">aptitude</span> <span class="token parameter variable">-vvvvv</span> moo
+<span class="token comment"># All right, you win.</span>
+<span class="token comment">#</span>
+<span class="token comment">#                               /----\\</span>
+<span class="token comment">#                       -------/      \\</span>
+<span class="token comment">#                      /               \\</span>
+<span class="token comment">#                     /                |</span>
+<span class="token comment">#   -----------------/                  --------\\</span>
+<span class="token comment">#   ----------------------------------------------</span>
+$ <span class="token function">aptitude</span> <span class="token parameter variable">-vvvvvv</span> moo
+<span class="token comment"># What is it?  It&#39;s an elephant being eaten by a snake, of course.</span>
+$ <span class="token function">aptitude</span> <span class="token parameter variable">-vvvvvvv</span> moo
+<span class="token comment"># What is it?  It&#39;s an elephant being eaten by a snake, of course.</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3);function v(u,b){const a=i("ExternalLinkIcon");return t(),l("div",null,[r,n("ul",null,[n("li",null,[n("a",p,[s("有趣的 Linux 命令"),o(a)])])]),d])}const g=e(m,[["render",v],["__file","funny-command.html.vue"]]);export{g as default};
