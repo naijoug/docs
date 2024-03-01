5efd8ff12c9a6aeb351ce7976610efb498c2d6e6
naijoug <naijoug@outlook.com>
2024-03-01 04:36:20 +0000

    Deploying to gh-pages from @ naijoug/docs@b841a0006982e29f0555f98ef5d8b36d8ee58b29 🚀

---
+++ assets/command.html-vS2oJi21.js
@@ -0,0 +1,27 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as e,a,f as l}from"./app-whABLkha.js";const i={},t=l(`<h2 id="commands" tabindex="-1"><a class="header-anchor" href="#commands" aria-hidden="true">#</a> Commands</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>$ flutter doctor <span class="token comment"># 检查 flutter 环境是否正确</span>
+<span class="token comment"># flutter create 创建工程</span>
+<span class="token comment">#  配置</span>
+<span class="token comment">#   -template=(app/module/package/plugin/skeleton) 创建工程模板</span>
+<span class="token comment">#   -platforms=(ios/android/windows/linux/macos/web) 指定平台</span>
+<span class="token comment">#   -a 指定 android 开发语言 (默认: kotlin)</span>
+<span class="token comment">#   -i 指定 ios 开发语言 (默认: swift)</span>
+$ flutter create <span class="token builtin class-name">test</span> <span class="token comment"># 创建 flutter test 项目</span>
+
+<span class="token comment"># 清理</span>
+$ flutter clean
+
+<span class="token comment"># 获取依赖</span>
+$ flutter pub get
+
+<span class="token comment"># 运行 flutter 程序</span>
+$ flutter run
+
+<span class="token comment"># 发布 packages</span>
+$ flutter packages pub publish --dry-run <span class="token comment"># 检查包</span>
+$ flutter packages pub pushlish <span class="token comment"># 发布包</span>
+
+<span class="token comment"># 构建 framework</span>
+$ flutter build ios-framework <span class="token parameter variable">--output</span><span class="token operator">=</span>path/to/
+<span class="token comment"># 构建 cocoapods framework</span>
+$ flutter build ios-framework <span class="token parameter variable">--cocoapods</span> <span class="token parameter variable">--output</span><span class="token operator">=</span>path/to/
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2);function c(o,r){return s(),e("div",null,[a(" more "),t])}const p=n(i,[["render",c],["__file","command.html.vue"]]);export{p as default};
