1ae147714de58612e3acfce923e14120ff7a25cb
naijoug <naijoug@outlook.com>
2024-07-30 00:50:42 +0000

    Deploying to gh-pages from @ naijoug/docs@35788b59a41d0005dd0f4c91311874e88eab4975 🚀

---
+++ assets/0x05.serializable.html-bOGQFSCZ.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as r,c as o,a as t}from"./app-K4IZJxSc.js";const a={};function c(_,l){return r(),o("div",null,[t(" more ")])}const m=e(a,[["render",c],["__file","0x05.serializable.html.vue"]]);export{m as default};
