2d435f526d18378b9722c2e6b85ece9b30ec0ba0
naijoug <naijoug@outlook.com>
2024-11-06 01:13:27 +0000

    Deploying to gh-pages from @ naijoug/docs@a6246a3181c01bc7912b16ae6f34c3e536ed5407 🚀

---
+++ assets/agent.html-l8QvZSi_.js
@@ -0,0 +1,10 @@
+import{_ as o}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as a,o as r,c as i,a as c,b as e,e as n,d as s,f as l}from"./app-bbmAsq4l.js";const d={},p=e("h2",{id:"reference",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#reference","aria-hidden":"true"},"#"),n(" reference")],-1),h={href:"https://screenshottocode.com/",target:"_blank",rel:"noopener noreferrer"},m={href:"https://github.com/abi/screenshot-to-code",target:"_blank",rel:"noopener noreferrer"},u=e("blockquote",null,[e("p",null,"Drop in a screenshot and convert it to clean code (HTML/Tailwind/React/Vue)")],-1),_={id:"gpt-engineer",tabindex:"-1"},f=e("a",{class:"header-anchor",href:"#gpt-engineer","aria-hidden":"true"},"#",-1),b={href:"https://github.com/AntonOsika/gpt-engineer",target:"_blank",rel:"noopener noreferrer"},v=l(`<blockquote><p>Specify what you want it to build, the AI asks for clarification, and then builds it.</p></blockquote><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token comment"># install </span>
+$ pip <span class="token function">install</span> gpt-engineer
+
+<span class="token comment"># 新建项目文件夹</span>
+$ <span class="token function">mkdir</span> project_folder
+<span class="token comment"># 创建项目 prompt 项目指令</span>
+$ <span class="token function">vim</span> project_folder/prompt
+<span class="token comment"># 运行 gpt-engineer</span>
+$ gpt-engineer project_folder/
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2);function g(k,x){const t=a("ExternalLinkIcon");return r(),i("div",null,[c(" more "),p,e("ul",null,[e("li",null,[e("a",h,[n("Screenshot to Code"),s(t)]),n(" 👉🏻 "),e("a",m,[n("🐙"),s(t)]),u])]),e("h2",_,[f,n(),e("a",b,[n("GPT Engineer"),s(t)])]),v])}const E=o(d,[["render",g],["__file","agent.html.vue"]]);export{E as default};
